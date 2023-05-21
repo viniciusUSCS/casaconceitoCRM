d06aea7333a63d9a9b2c952566ce2e42564db2b7
Vinicius Alexandre <113480438+viniciusUSCS@users.noreply.github.com>
2023-05-21 15:38:25 -0300

    Add files via upload

---
+++ MENU CAPTADOR/cadastrar_cliente/scripts/scripts.js
@@ -0,0 +1,16 @@
+
+    function formpessoafisica(){
+        document.getElementById("form-pessoa_fisica").style.display = 'block'
+        document.getElementById("form-pessoa_juridica").style.display="none"
+
+        
+        
+        
+    }
+
+    function formpessoajuridica(){
+        document.getElementById("form-pessoa_fisica").style.display = 'none'
+        document.getElementById("form-pessoa_juridica").style.display="block"
+        
+        
+    }
